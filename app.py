9e70e4129ae5149152a10cbd605b16224327fe80
Zlata Filcheva <fzlata6@gmail.com>
2022-08-01 19:34:11 +0300

    ytyyuyuyu

--- app.py
+++
@@ -1,3 +0,0 @@
-thyyuyt
-
-tyuiyt
--- hello world.py
+++
@@ -1,8 +0,0 @@
-chachter_name = 'Phill'
-print('A married couple argue one morning before work')
-print('One of their sons, ' + chachter_name + ', fakes being sick so he can stay home by himself.')
-print('He ends up being bored. ')
-print('He looks through his parents room, trying to get some insight into romantic matters.')
-print('He decides to set out for Birch Creek to do some fishing')
-chachter_age = '5'
-print(' He was only ' + chachter_age + ' years old when this happend')
